49a5c8db2e6d300b5b9e928ca9b48006f5139fa4
Marina Berdnikova <berdni4ka@rambler.ru>
2023-04-09 16:55:28 +0300

    Add homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	notes text NOT NULL
+);
+SELECT * FROM employees
+
+CREATE TABLE customers(
+	customer_id varchar(50) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+SELECT * FROM customers
+
+CREATE TABLE orders(
+	order_id int PRIMARY KEY NOT NULL,
+	customer_id varchar(50) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_data date NOT NULL,
+	city varchar(100) NOT NULL
+);
